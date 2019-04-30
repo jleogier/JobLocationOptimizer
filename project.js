daba96fff91bc803a4cdf0afe6fbadb0e2ad83fe
jleogier <jleogier@users.noreply.github.com>
2019-04-30 14:04:44 -0700

    Add files via upload

--- project.js
+++ project.js
@@ -5,22 +5,67 @@ $(function() {
     const GMAPS_API_KEY = 'AIzaSyDugko09xbH7YY4avMUTZJNsA3uKHcuTeI';
     var map;
 
-// Working with the Adzuna API
-
     const ADZUNA_BASE_URL = 'http://api.adzuna.com/v1/api/';
     const ADZUNA_KEY = 'b22271cad1b74c76f8c6ec3587c34e86';
     const ADZUNA_APP_ID = 'e76d0c45';
-    
+
+
     // https://api.adzuna.com/v1/api/jobs/us/history?app_id=e76d0c45&app_key=b22271cad1b74c76f8c6ec3587c34e86&location0=UK&location1=london&category=it-jobs&content-type=application/json
 
     // http://api.adzuna.com/v1/api/property/gb/search/1?app_id={YOUR_APP_ID}&app_key={YOUR_APP_KEY}
 
+    // https://api.adzuna.com:443/v1/api/jobs/gb/history?app_id=e76d0c45&app_key=b22271cad1b74c76f8c6ec3587c34e86&where=london&category=engineering-jobs
 
+    // https://api.adzuna.com:443/v1/api/jobs/us/search/1?app_id=e76d0c45&app_key=b22271cad1b74c76f8c6ec3587c34e86&where=san%20francisco&category=it-jobs
+
+
+
+
+    // Gets general Job Search Data
+    function AdzunaJobSearch (userInputCountry, userInputCity, jobCategory){
+        let AdzunaJobSearchResponse = ADZUNA_BASE_URL + `jobs/${userInputCountry}/search/1?app_id=${ADZUNA_APP_ID}&app_key=${ADZUNA_KEY}&where=${userInputCity}&category=${jobCategory}&content-type=application/json`;
+
+        console.log('The Job Search Response is:', AdzunaJobSearchResponse);
+
+        return fetch (AdzunaJobSearchResponse, {
+            headers: {
+                Accept: 'application/json'
+            }
+        })
+        .then (response => /*Error Handler*/ response.json())
+    }
+
+
+    // Gets Total Job Count and Salary mean from search
+    function AdzunaJobSearchCountnSalary (data) {                
+        
+        let jobCount = data.count;
+        let jobSalaryMean = data.mean;
+
+        console.log('Job Count is:', jobCount);
+        console.log('Job Salary Mean is:', jobSalaryMean);
+
+        let jobCountnSalaryData = [jobCount, jobSalaryMean];
+
+        return jobCountnSalaryData;
+    }
+
+
+    // Converts Total Count and Salary Mean response into HTML
+    function jobCountnSalaryHTML (jobCountnSalaryData) {
+        let jobCountSalaryHTML = Object.keys(jobCountnSalaryData);
+
+        return `<div>${jobCountSalaryHTML}</div>`
+    }
+
+
+
+    // Gets Job History Data
     function getAdzunaJobHistory (userInputCountry, userInputCity, jobCategory) {
         
-        let adzunaHistoryResponse = ADZUNA_BASE_URL + `jobs/gb/history?app_id=${ADZUNA_APP_ID}&app_key=${ADZUNA_KEY}&location0=${userInputCountry}&location1=${userInputCity}&category=${jobCategory}&content-type=application/json`;
+        let adzunaHistoryResponse = ADZUNA_BASE_URL + `jobs/${userInputCountry}/history?app_id=${ADZUNA_APP_ID}&app_key=${ADZUNA_KEY}&where=${userInputCity}&category=${jobCategory}&content-type=application/json`;
 
-        console.log(adzunaHistoryResponse);
+        console.log('The History Response is:', adzunaHistoryResponse);
 
          return fetch (adzunaHistoryResponse, {
              headers: {
@@ -29,29 +74,74 @@ $(function() {
         })
         .then (response => /*Error Handler*/ response.json())           
     }
+    
 
+    // Displays results in HTLM
     function displayResults (data) {        
-        $('#results').html(historyHTML(data.month));
+        $('#results').html(historyHTML(data.month)).append(jobCountnSalaryHTML(jobCountnSalaryData));
     }
 
+
+    // Converts Job History Data to HTML 
     function historyHTML (months) {
         let historyHTML = Object.keys(months).map(key => historyMonthHTML (key, months[key])).join(`\n`);
-        return `<div>History:${historyHTML}</div>`
+        return `<div>History:<ol>${historyHTML}</ol></div>`
     }
 
+
+    // Formats Job History info
     function historyMonthHTML (month, salary) {
-        return `<div>${month} salary: ${salary}</div>`
+
+        // ADD SORT FUNCTIONALITY 
+
+        return `<li>${month} salary: ${salary}</li>`
     }
+
 
 
     // Initializes Map 
     function initMap() {
       map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 34.052234, lng: -118.243685},
+        // center: {lat: 34.052234, lng: -118.243685},
+        center: {lat: 37.77439, lng: -122.419416},
         zoom: 9
-      });
+        });
+
+
+        infoWindow = new google.maps.InfoWindow;
+
+        // Get User Location
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+              var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+              };
+  
+              infoWindow.setPosition(pos);
+              infoWindow.setContent('Location found.');
+              infoWindow.open(map);
+              map.setCenter(pos);
+            }, function() {
+              handleLocationError(true, infoWindow, map.getCenter());
+            });
+        } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+        }
     }
 
+
+    // Update Map
+
+    function updateMap () {
+
+    }
+
+    // Convert user input into Geo Coordinates 
+
+
+    // Submit Button 
 
     function submitHandler () {
 
@@ -60,32 +150,33 @@ $(function() {
             e.preventDefault();
 
             // Gets User Input
-
-            let userInputCountry = $('#userInputCountry').val().trim();
+            let userInputCountry = $('#userInputCountry').val();
             let userInputCity = $('#userInputCity').val().trim();
-            let jobCategory = $('#userInputJobCategory').val().trim();
+            let jobCategory = $('#userInputJobCategory').val();
 
             // Logs User Input
-        
             console.log('Submit Button was clicked');
             console.log('This is the captured User Input for the COUNTRY:', userInputCountry);
             console.log('This is the captured User Input for the CITY:', userInputCity);
             console.log('This is the captured User Input for the JOB CATEGORY:', jobCategory);
 
+
             // Does the things
-
             getAdzunaJobHistory (userInputCountry, userInputCity, jobCategory)
+            AdzunaJobSearch (userInputCountry, userInputCity, jobCategory)
             .then(data => displayResults(data));
-
         });
     }
-
- 
 
 
 
     getAdzunaJobHistory();
+    AdzunaJobSearch();
+    AdzunaJobSearchCount();
 
+    AdzunaJobSearchCountnSalary();
+
+    jobCountnSalaryHTML();
     submitHandler();
 
     initMap ();
